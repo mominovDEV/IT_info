f1143d68c94e7826b94ac42e386b334116c0d677
MominovDEV <mardonmominov6@gmail.com>
2023-06-19 14:28:54 +0500

    prefix added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,20 +1,27 @@
 const { Router } = require("express");
-
-const router = Router();
+const express = require("express");
 
 const dictionaryRoute = require("./dictionary.routes"); //
 const categoryRoute = require("./category.routes"); //
 const authorRoute = require("./author.routes");
 const adminRoute = require("./admin.routes");
-const descriptionRout = require('./description.routes');
+const descriptionRout = require("./description.routes");
 
+express.Router.prefix = function (path, subRouter) {
+  const router = Router();
+  this.use(path, router);
+  subRouter(router);
+  return router;
+};
 
-router.use("/dictionary", dictionaryRoute);
-router.use("/description", descriptionRout);
-router.use("/category", categoryRoute);
-router.use("/author", authorRoute);
-router.use("/admin", adminRoute);
+const router = Router();
 
-
+router.prefix("/api", (apiRouter) => {
+  apiRouter.use("/dictionary", require('./dictionary.routes'));
+  apiRouter.use("/description", require('./description.routes'));
+  apiRouter.use("/category", require('./category.routes'));
+  apiRouter.use("/author", require('./author.routes'));
+  apiRouter.use("/admin", require('./admin.routes'));
+});
 
 module.exports = router;
