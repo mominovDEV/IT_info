68773c5e1228ffb7f27db3e34264430771d8803f
MominovDEV <mardonmominov6@gmail.com>
2023-06-22 21:32:23 +0500

    loggerMiddelware added

--- middleware/loggerMiddelware.js
+++ middleware/loggerMiddelware.js
@@ -1,31 +1,31 @@
+const expressWinston = require("express-winston");
+const {format, transports } = require("winston");
+const { metadata, json, prettyPrint} = format;
 const { logger, errorLogger } = require("express-winston");
 const winston = require("winston");
 const config = require("config");
 require("winston-mongodb");
 
-const {format, transports } = require("winston");
-const { metadata, json, prettyPrint} = format;
-
-const logPolice = logger({
+const expressWinstonLogger = expressWinston.logger({
   transports: [
-    new transports.Console(),
-    new transports.MongoDB({
-      db: config.get("ATLAS_URI"),
-      options: { useUnifiedTopology: true },
-      metaKey: "meta",
-    }),
+      new transports.MongoDB({
+          db: config.get("ATLAS_URI"),
+          options: { useUnifiedTopology: true },
+          metaKey: "meta",
+        }),
+        new transports.Console(),
   ],
-  format: format.combine(json(), prettyPrint(), metadata()),
-  meta: true, 
-  msg: "HTTP {{req.method}} {{req.url}}", 
+  format: combine(timestamp(), prettyPrint(), metadata()),
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}",
   expressFormat: true,
-  colorize: false, 
+  colorize: false,
   ignoreRoute: function (req, res) {
     return false;
-  }, 
+  },
 });
 
-const errLogger = errorLogger({
+const expressWinstonErrorLogger = errorLogger({
   transports: [
     new transports.Console(),
 
@@ -40,6 +40,6 @@ const errLogger = errorLogger({
 });
 
 module.exports = {
-  logPolice,
-  errLogger,
+  expressWinstonLogger,
+  expressWinstonErrorLogger,
 };
